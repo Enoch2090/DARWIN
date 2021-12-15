e526edc64af8d045928c37a5571061cfb0bc26f8
enoch2090 <gyc990926@gmail.com>
2021-12-15 02:50:28 -0500

    heroku deployment

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
-
+pip3 install torch==1.10.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 heroku buildpacks:add heroku/jvm
 python3 -m spacy download en_core_web_sm
